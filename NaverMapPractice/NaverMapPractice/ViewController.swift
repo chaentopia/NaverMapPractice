ef92dcfa30c71b642b50c1776f6958f4dc21fb14
chaentopia <cce523@hanmail.net>
2023-12-23 15:39:28 +0900

    chore: 초기 세팅

---
+++ NaverMapPractice/NaverMapPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaverMapPractice
+//
+//  Created by 정채은 on 12/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
